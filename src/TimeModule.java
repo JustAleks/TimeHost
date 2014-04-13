36f2fbf06ed0659897cb692375376a82fe88f367
JustAleks <dj_gnidovskiy@mail.ru>
2014-04-14 00:37:02 +0300

    Small fix

--- src/TimeModule.java
+++ src/TimeModule.java
@@ -1,6 +1,3 @@
-/**
- * Created by Optimus on 13.04.2014.
- */
 
 import java.io.*;
 import java.net.*;
@@ -19,21 +16,15 @@ public class TimeModule {
 
         try {
             ser = new ServerSocket(8020);
-	/*
-	 * This will wait for a connection to be made to this socket.
-         */
             soc = ser.accept();
             InputStream inputStream = soc.getInputStream();
             ObjectInput input = new ObjectInputStream(inputStream);
             str = (String) input.readObject();
-            System.out.println(str);
-            if (str != null){
+            if (str.equals(etalon)){
                 now = new Date();
-
                 DateFormat formatter = new SimpleDateFormat("HH:mm:ss");
                 String s = formatter.format(now);
                 System.out.println("Current time: " + s);
-
                 OutputStream outputStream = soc.getOutputStream();
                 ObjectOutput output = new ObjectOutputStream(outputStream);
                 output.writeObject("Current time: " + s);
@@ -41,10 +32,6 @@ public class TimeModule {
                 output.close();
             }
             input.close();
-
-
-            // print out what we just received
-
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
